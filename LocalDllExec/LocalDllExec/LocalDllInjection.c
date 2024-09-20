e912530bc3a37108139e4f9e1e5804374bdb1838
alex14324 <36421715+alex14324@users.noreply.github.com>
2024-09-20 22:59:06 +0700

    Add files via upload

---
+++ LocalDllExec/LocalDllExec/LocalDllInjection.c
@@ -0,0 +1,35 @@
+// @Pentesterclubpvtltd | @alex14324 : Pentesterclubpvtltd
+
+#include <Windows.h>
+#include <stdio.h>
+
+
+
+
+int main(int argc, char* argv[]) {
+
+	// Checking the command line arguments
+	if (argc < 2){
+		printf("[!] Missing Argument; Dll Payload To Run \n");
+		return -1;
+	}
+
+	printf("[i] Injecting \"%s\" To The Local Process Of Pid: %d \n", argv[1], GetCurrentProcessId());
+	
+
+	// https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibrarya
+	printf("[+] Loading Dll ... ");
+	if (LoadLibraryA(argv[1]) == NULL) {
+		printf("[!] LoadLibraryA Failed With Error : %d \n", GetLastError());
+		return -1;
+	}
+	printf("[+] DONE ! \n");
+
+
+	
+	printf("[#] Press <Enter> To Quit ... ");
+	getchar();
+
+	return 0;
+}
+
